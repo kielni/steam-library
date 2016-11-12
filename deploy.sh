aaa1553071154e761bd5700f0bf716f0a55ab912
Kimberly Nicholls <kimberly@gridium.com>
2016-11-11 17:27:24 -0800

    assets are not gzipped yet

--- deploy.sh
+++ deploy.sh
@@ -10,10 +10,10 @@ echo "* assets except .css, .js and .html"
 $SYNC --acl public-read --cache-control 'max-age=31536000, public' --exclude '*.html' --exclude '*.css' --exclude '*.js' --exclude 'build.txt'
 echo
 echo "* css"
-$SYNC --acl public-read --cache-control 'max-age=31536000, public' --exclude '*' --include '*.css' --content-encoding gzip --content-type text/css
+$SYNC --acl public-read --cache-control 'max-age=31536000, public' --exclude '*' --include '*.css' --content-type text/css
 echo
 echo "* js"
-$SYNC --acl public-read --cache-control 'max-age=31536000, public' --exclude '*' --include '*.js' --content-encoding gzip --content-type application/js
+$SYNC --acl public-read --cache-control 'max-age=31536000, public' --exclude '*' --include '*.js' --content-type application/js
 echo
 echo "* html"
 $SYNC --acl public-read --cache-control 'no-cache' --exclude '*' --include '*.html' --include 'build.txt'
