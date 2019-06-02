676d8b3422cf128e442a34f0e8bfdc7335e200b2
Kimberly Nicholls <1649528+kielni@users.noreply.github.com>
2019-06-02 10:18:15 -0700

    render incrementally on initial load
    remove vertical-collection since it breaks actions/links inside components
    for initial render, draw only 9 cards (a screen and a half), then render the rest of the cards in batches
    do this only once; later updates don't need to re-render

--- app/components/games-list.js
+++ app/components/games-list.js
@@ -1,6 +1,40 @@
 import Component from '@ember/component';
+import { run } from '@ember/runloop';
 
 export default Component.extend({
+  chunkSize: 9,
+  fullyRendered: false,
+
+  didReceiveAttrs() {
+    const size = this.get('chunkSize');
+    const all = this.get('games') || [];
+
+    this._super(...arguments);
+    if (this.get('fullyRendered')) {
+      this.set('displayGames', all);
+    } else {
+      this.set('displayGames', all.slice(0, size));
+      this.set('start', (new Date()).getTime());
+      this.incrementalRender(size + size);
+    }
+  },
+
+  incrementalRender(size) {
+    const all = this.get('games') || [];
+
+    run.next(this, function() {
+      console.log('incrementalRender: ', size);
+      this.set('displayGames', all.slice(0, size));
+      if (size < all.length) {
+        this.incrementalRender(size + this.get('chunkSize'));
+      } else {
+        this.set('fullyRendered', true);
+        console.log('rendered in ', ((new Date()).getTime() - this.get('start'))/1000, 's');
+      }
+    });
+
+  },
+
   actions: {
     filterTag: function(tag) {
       this.filterTag(tag);
