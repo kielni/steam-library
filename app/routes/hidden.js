8c499831a4af45b647606b7e54aa1bad574cebb2
Kimberly Nicholls <1649528+kielni@users.noreply.github.com>
2019-06-09 09:29:05 -0700

    add link to hidden

--- app/routes/hidden.js
+++ app/routes/hidden.js
@@ -1,13 +1,15 @@
-import $ from 'jquery';
+import { inject } from '@ember/service';
 import Route from '@ember/routing/route';
 import config from 'steam-library/config/environment';
 import ParsesGame from 'steam-library/mixins/parses-game';
 
 export default Route.extend(ParsesGame, {
+  ajax: inject(),
+
   model() {
     const hidden = JSON.parse(localStorage.getItem('hidden') || '[]');
 
-    return $.ajax(config.APP.LIBRARY).then((response) => {
+    return this.get('ajax').request(config.APP.LIBRARY).then((response) => {
       return {
         games: response.data.map(game => this.normalize(game, hidden, []))
           .filter(game => !game.get('ageRestricted') && game.get('hidden'))
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,19 +1,24 @@
-import $ from 'jquery';
+import { inject } from '@ember/service';
 import Route from '@ember/routing/route';
 import config from 'steam-library/config/environment';
 import ParsesGame from 'steam-library/mixins/parses-game';
 
 export default Route.extend(ParsesGame, {
+  ajax: inject(),
+
   model() {
     const hidden = JSON.parse(localStorage.getItem('hidden') || '[]');
     const starred = JSON.parse(localStorage.getItem('starred') || '[]');
 
     console.log('loading', config.APP.LIBRARY);
 
-    return $.ajax(config.APP.LIBRARY).then((response) => {
+    return this.get('ajax').request(config.APP.LIBRARY).then((response) => {
+      const games = response.data.map(game => this.normalize(game, hidden, starred));
+
+      this.set('hiddenCount', games.filter(game => game.hidden).length);
+
       return {
-        games: response.data.map(game => this.normalize(game, hidden, starred))
-          .filter(game => !game.get('ageRestricted') && !game.get('hidden'))
+        games: games.filter(game => !game.get('ageRestricted') && !game.get('hidden'))
           .sortBy('index')
       };
     });
@@ -22,5 +27,6 @@ export default Route.extend(ParsesGame, {
   setupController(controller, model) {
     this._super(controller, model);
     controller.set('filters', JSON.parse(localStorage.getItem('filters') || '{}'));
+    controller.set('hiddenCount', this.get('hiddenCount'));
   }
 });
